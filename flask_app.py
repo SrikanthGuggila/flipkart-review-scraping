18a099851adcf4da4b06f35965a91a9453079f1b
Srikanth <srikanthguggila2@gmail.com>
2022-04-08 07:37:35 +0530

    Project initial code uploaded

---
+++ flask_app.py
@@ -0,0 +1,83 @@
+# doing necessary imports
+
+from flask import Flask, render_template, request
+# from flask_cors import CORS,cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+import pymongo
+
+app = Flask(__name__)  # initialising the flask app with the name 'app'
+
+
+
+
+@app.route('/',methods=['POST','GET']) # route with allowed methods as POST and GET
+def index():
+    if request.method == 'POST':
+        searchString = request.form['content'].replace(" ","")# obtaining the search string entered in the form
+        print('search string is: ',searchString)
+        try:
+            searchString = str(searchString)
+            dbConn = pymongo.MongoClient("mongodb://localhost:27017/")# opening a connection to Mongo
+            db = dbConn['crawlerDB'] # connecting to the database called crawlerDB
+            reviews = db[searchString]# searching the collection with the name same as the keyword
+            if reviews.count_documents({}) > 0: # if there is a collection with searched keyword and it has records in it
+                return render_template('results.html',reviews=reviews) # show the results to user
+            else:
+                flipkart_url = "https://www.flipkart.com/search?q=" + searchString # preparing the URL to search the product on flipkart
+                uClient = uReq(flipkart_url)# requesting the webpage from the internet
+                flipkartPage = uClient.read() # reading the webpage
+                uClient.close() # closing the connection to the web server
+                flipkart_html = bs(flipkartPage, "html.parser") # parsing the webpage as HTML
+                bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"}) # seacrhing for appropriate tag to redirect to the product link
+                del bigboxes[0:3] # the first 3 members of the list do not contain relevant information, hence deleting them.
+                box = bigboxes[0] #  taking the first iteration (for demo)
+                productLink = "https://www.flipkart.com" + box.div.div.div.a['href'] # extracting the actual product link
+                prodRes = requests.get(productLink) # getting the product page from server
+                prod_html = bs(prodRes.text, "html.parser") # parsing the product page as HTML
+                commentboxes = prod_html.find_all('div', {'class': "col _2wzgFH"}) # finding the HTML section containing the customer comments
+
+                table = db[searchString] # creating a collection with the same name as search string. Tables and Collections are analogous.
+                #filename = searchString+".csv" #  filename to save the details
+                #fw = open(filename, "w") # creating a local file to save the details
+                #headers = "Product, Customer Name, Rating, Heading, Comment \n" # providing the heading of the columns
+                #fw.write(headers) # writing first the headers to file
+                reviews = [] # initializing an empty list for reviews
+                #  iterating over the comment section to get the details of customer and their comments
+                for commentbox in commentboxes:
+                    try:
+                        name = commentbox.find_all('p',{'class': "_2sc7ZR _2V5EHH"})[0].text
+                        print(name)
+                    except:
+                        name = 'No Name'
+
+                    try:
+                        rating = commentbox.div.div.text
+                        print(rating)
+                    except:
+                        rating = 'No Rating'
+
+                    try:
+                        commentHead = commentbox.p.text
+                        print(commentHead)
+                    except:
+                        commentHead = 'No Comment Heading'
+                    try:
+                        custComment = commentbox.find_all('div',{'class': "t-ZTKy"})[0].div.div.text
+                        print(custComment)
+                    except:
+                        custComment = 'No Customer Comment'
+                    #fw.write(searchString+","+name.replace(",", ":")+","+rating + "," + commentHead.replace(",", ":") + "," + custComment.replace(",", ":") + "\n")
+                    mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
+                              "Comment": custComment} # saving that detail to a dictionary
+                    x = table.insert_one(mydict) #insertig the dictionary containing the rview comments to the collection
+                    reviews.append(mydict) #  appending the comments to the review list
+                return render_template('results.html', reviews=reviews) # showing the review to the user
+        except:
+            return 'something is wrong'
+            #return render_template('results.html')
+    else:
+        return render_template('index.html')
+if __name__ == "__main__":
+    app.run(port=8000,debug=True) # running the app on the local machine on port 8000
